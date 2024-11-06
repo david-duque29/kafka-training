87ce7e3485b002df3f9711d3334caf96ba37d7ff
david-duque29 <ivan.duque@quind.io>
2024-11-06 10:09:52 -0500

    avance: Se agrega ObjectMapper en en la clase RefundConsumer por la configuracion de serialization.StringDeserializer del value-deserializer consumer en el yaml. ya que recibe un string y no un objeto Refund

--- src/main/java/co/example/kafkatraining/consumer/RefundConsumer.java
+++ src/main/java/co/example/kafkatraining/consumer/RefundConsumer.java
@@ -2,6 +2,8 @@ package co.example.kafkatraining.consumer;
 
 import co.example.kafkatraining.handler.RefundHandler;
 import co.example.kafkatraining.schemas.Refund;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,11 +14,17 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class RefundConsumer {
     private final RefundHandler refundHandler;
+    private final ObjectMapper objectMapper;
 
-    @KafkaListener(topics ="REFUND", groupId = "refund-group")
-    public void recieveRefundMessage(Refund refund){
-        log.info("Received refund message: {}", refund);
-        refundHandler.manageRefund(refund);
+    @KafkaListener(topics = "REFUND", groupId = "refund-group")
+    public void recieveRefundMessage(String refundJson){
+        try {
+            Refund refund = objectMapper.readValue(refundJson, Refund.class);
+            log.info("Received refund message: {}", refund);
+            refundHandler.manageRefund(refund);
+        } catch (JsonProcessingException e) {
+            log.error("Error parsing refund message", e);
+        }
     }
-
 }
+
